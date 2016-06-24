0cb8962710bd0fc5f86dbb601c75f394d242348c
Wesley <wesleythompson1995@gmail.com>
2016-06-24 11:26:31 -0500

    Add destruction, disappear scripts for destructible environments. Create destruction test scene.

---
+++ Assets/Scripts/DestructableObject.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestructableObject : MonoBehaviour {
+	[Tooltip("The tag of the object you want to all to destroy this object")]
+	[SerializeField] private string destructorTag;
+	[Tooltip("The prefab you want to replace this object with on destruction")]
+	[SerializeField] private GameObject destroyedObject;
+
+	void OnCollisionEnter (Collision collision){
+		if (collision.gameObject.tag == destructorTag) 
+		{
+			Instantiate (destroyedObject, transform.position, transform.rotation);
+			Destroy (gameObject);
+		}
+	}
+}
