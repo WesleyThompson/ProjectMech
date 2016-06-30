255a0d8ac508763092db743fd623e023dfba5466
Alex Wilhelm <awwilhelm2014@gmail.com>
2016-06-30 16:50:30 -0500

    Enemy walks toward player

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+
+    private NavMeshAgent agent;
+    public GameObject moveTarget;
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();//transform.FindChild("NavMesh").
+        // agent.updateRotation = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        RaycastHit hit;
+        Vector3 playerDirection = transform.position - moveTarget.transform.position;
+        //Debug.DrawRay(transform.position, playerDirection, Color.blue);
+
+        playerDirection *= -1;
+
+        if (Physics.Raycast(transform.position, playerDirection, out hit))
+        {
+            print(hit.transform.parent.name);
+        }
+        if (hit.transform.parent.name != "PlayerTemp")
+        {
+            agent.SetDestination(moveTarget.transform.position);
+        } else
+        {
+            agent.SetDestination(transform.position);
+        }
+    }
+}
---
+++ Assets/Scripts/EnemyScan.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyScan : MonoBehaviour {
+
+    [Range(0, 45)]
+    public float scanRange = 45;
+    public float scanTime = 1f;
+    private int scanDirection = -1;
+    private Vector3 centerRotation;
+    private Vector3 startRotation;
+    private Vector3 endRotation;
+    private float startTime;
+    private Transform topTransform;
+
+	void Awake ()
+    {
+        topTransform = transform.FindChild("Top");
+        centerRotation = topTransform.localEulerAngles;
+        print(centerRotation);
+        topTransform.localEulerAngles = new Vector3(topTransform.localEulerAngles.x, centerRotation.y, topTransform.localEulerAngles.z);
+        endRotation = startRotation = topTransform.localEulerAngles;
+        startTime = Time.time;
+	}
+	
+	void Update ()
+    {
+        if(RoughlyEqual(topTransform.localEulerAngles.y, endRotation.y, 0.001f))
+        {
+            if (RoughlyEqual(topTransform.localEulerAngles.y, centerRotation.y, 0.001f))
+            {
+                startRotation = topTransform.localEulerAngles;
+                endRotation = new Vector3(topTransform.localEulerAngles.x, centerRotation.y + scanRange * scanDirection, topTransform.localEulerAngles.z);
+            }
+            else
+            {
+                scanDirection *= -1;
+                startRotation = topTransform.localEulerAngles;
+                endRotation = new Vector3(topTransform.localEulerAngles.x, centerRotation.y, topTransform.localEulerAngles.z);
+            }
+            startTime = Time.time;
+        }
+        float tRot = Mathf.LerpAngle(startRotation.y, endRotation.y, (Time.time - startTime) / scanTime);
+        topTransform.localEulerAngles = new Vector3(topTransform.localEulerAngles.x, tRot, topTransform.localEulerAngles.z);
+    }
+
+    bool RoughlyEqual(float firstVal, float secondVal, float buffer)
+    {
+        while (firstVal >= 360)
+        {
+            firstVal -= 360;
+        }
+        while (firstVal < 0)
+        {
+            firstVal += 360;
+        }
+
+        while (secondVal >= 360)
+        {
+            secondVal -= 360;
+        }
+        while (secondVal < 0)
+        {
+            secondVal += 360;
+        }
+        return Mathf.Abs(firstVal - secondVal) < buffer;
+    }
+}
