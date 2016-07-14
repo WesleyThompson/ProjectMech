93a4e4e28a54cdbf4c6e4f66765983ed0e6ab6bb
Joshua Lewis <jil5x4@mail.missouri.edu>
2016-07-14 18:51:14 -0500

    Add files via upload

---
+++ Assets/Scripts/FrontLeftEngine.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FrontLeftEngine : MonoBehaviour 
+{
+
+	public GameObject dropship;
+	public Rigidbody rb;
+	private float curSpeed;
+
+	// Update is called once per frame
+	void Update ()
+	{
+		rightTurn ();
+
+		//Debug.Log (rb.angularVelocity.magnitude);
+		//if the dropship is rotating
+		/*if (rb.angularVelocity.magnitude != 0)
+			{
+			Debug.Log ("im turning");
+			//if we rotate left
+
+		}
+		//if we aren't turning then we may be moving
+		else if (curSpeed > 0) {
+			forward ();
+		}
+		//if we aren't moving or tunring, the we are idle
+		else
+		{
+			idle ();
+		}
+*/
+	}
+
+	void rightTurn()
+	{
+
+		transform.Rotate (Vector3.left);
+	}
+
+	void leftTurn()
+	{
+	
+
+	}
+
+	void forward()
+	{
+
+
+	}
+
+	void idle()
+	{
+	
+	}
+}
---
+++ Assets/Scripts/drop.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class drop : MonoBehaviour {
+	public moveTo dropShip;
+	public Rigidbody rb;
+	// Update is called once per frame
+	void Update () {
+		if (dropShip.getTimeAtDrop () > 4)
+		{
+			transform.parent = null;
+			rb.useGravity = true;
+		}
+
+	
+	}
+}
