773ff0fdadb072512c13e888e8b56f8ec68dafd2
Wesley Thompson <wesleythompson1995@gmail.com>
2016-07-23 17:23:31 -0500

    Finish DebrisController script (not currently using object pooling but could very easily). Adjust building prefabs.

--- Assets/Scripts/DebrisController.cs
+++ Assets/Scripts/DebrisController.cs
@@ -2,14 +2,38 @@
 using System.Collections;
 
 public class DebrisController : MonoBehaviour {
+    public float colliderOffDelay;
+    public float objectDoneDelay;
 
-	// Use this for initialization
-	void Start () {
-	
+    private float startTime;
+    private float colliderOffTime;
+    private float objectDoneTime;
+
+    //So we don't burn useless cycles
+    private bool colliderOff;
+    private bool objectDone;
+
+    private Collider[] colliders;
+
+    void Start () {
+        startTime = Time.time;
+        colliderOffTime = startTime + colliderOffDelay;
+        objectDoneTime = colliderOffTime + objectDoneDelay;
+        colliderOff = objectDone = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if (Time.time >= colliderOffTime && !colliderOff) {
+            colliders = gameObject.GetComponentsInChildren<Collider>();
+            foreach (Collider col in colliders) {
+                col.enabled = false;
+            }
+            colliderOff = true;
+        }
+        if (Time.time >= objectDoneTime && !objectDone) {
+            Destroy(gameObject);
+            objectDone = true;
+        }
 	}
 }
